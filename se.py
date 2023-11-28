5a7dbc4d74009caba036e63ee2506b53e608e929
mahendran-narayanan <mahendranNNM@gmail.com>
2023-11-29 00:41:10 +0530

    Added SE and SE_v2

---
+++ se.py
@@ -0,0 +1,18 @@
+class SE(tf.keras.Model):
+    def __init__(self,out):
+        super().__init__()
+        self.out = out
+        self.reduction = 32
+        self.squeeze = tf.keras.layers.GlobalAveragePooling2D()
+        self.func1 = tf.keras.layers.Dense(self.out//self.reduction,activation='relu')
+        self.func2 = tf.keras.layers.Dense(self.out,activation='sigmoid')
+
+    def call(self,x):
+        inp=x
+        x = self.squeeze(x)
+        x = self.func1(x)
+        x = self.func2(x)
+        x = tf.reshape(x,[-1,1,1,self.out])
+        sc = tf.keras.layers.multiply([inp,x])
+        res = tf.keras.layers.add([inp,sc])
+        return res
---
+++ se_v2.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+
+class SEv2(tf.keras.Model):
+    def __init__(self,out):
+        super().__init__()
+        self.out = out
+        self.reduction = 32
+        self.squeeze = tf.keras.layers.GlobalAveragePooling2D()
+        self.func1a = tf.keras.layers.Dense(self.out//self.reduction,activation='relu')
+        self.func1b = tf.keras.layers.Dense(self.out//self.reduction,activation='relu')
+        self.func1c = tf.keras.layers.Dense(self.out//self.reduction,activation='relu')
+        self.func1d = tf.keras.layers.Dense(self.out//self.reduction,activation='relu')
+        self.func2 = tf.keras.layers.Dense(self.out,activation='sigmoid')
+
+    def call(self,x):
+        inp=x
+        x = self.squeeze(x)
+        x1 = self.func1a(x)
+        x2 = self.func1b(x)
+        x3 = self.func1c(x)
+        x4 = self.func1d(x)
+        x = tf.concat([x1,x2,x3,x4],1)
+        x = self.func2(x)
+        x = tf.reshape(x,[-1,1,1,self.out])
+        sc = tf.keras.layers.multiply([inp,x])
+        res = tf.keras.layers.add([inp,sc])
+        return res
